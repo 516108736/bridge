45872d926a18ad457c02f89485fffc374ad35230
516108736 <516108736@qq.com>
2021-03-26 15:16:39 +0800

    dsa

--- contracts/contracts/ScfToken.sol
+++ contracts/contracts/ScfToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ScfToken is ERC20{
-    constructor()ERC20("SSCCFF","SCF"){
+    constructor ()  ERC20("SSCCFF","SCF") {
         _mint(msg.sender,100000*(10**uint256(decimals())));
     }
 }
